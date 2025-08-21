3b8fd18954a1404789cc00c53de02ce6fadec19c
Philippe Martin <phmartin@redhat.com>
2025-08-21 11:14:46 +0200

    feat: discover github feature

--- pkg/cmd/modules.go
+++ pkg/cmd/modules.go
@@ -4,5 +4,6 @@ import (
 	_ "github.com/manusa/ai-cli/pkg/inference/gemini"
 	_ "github.com/manusa/ai-cli/pkg/inference/ollama"
 	_ "github.com/manusa/ai-cli/pkg/tools/fs"
+	_ "github.com/manusa/ai-cli/pkg/tools/github"
 	_ "github.com/manusa/ai-cli/pkg/tools/kubernetes"
 )
